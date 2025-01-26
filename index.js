d27f0096ee841ad284489542a70ca1344e015432
Jan Radtke <jan-radtke@gmx.de>
2025-01-26 01:06:02 +0100

    take submit information from request

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-const express = require('express');
-const axios = require('axios');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,23 +14,32 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
-app.get('/', async (req, res) => {
-	return res.render('index', { title: 'Home | HubSpot APIs' });
-})
+app.get("/", async (req, res) => {
+	return res.render("index", { title: "Home | HubSpot APIs" });
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-app.get('/update-cobj', async (req, res) => {
-	return res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
-})
+app.get("/update-cobj", async (req, res) => {
+	return res.render("updates", { title: "Update Custom Object Form | Integrating With HubSpot I Practicum" });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-app.post('/update-cobj', async (req, res) => {
-	return res.redirect('/update-cobj');
-})
+app.post("/update-cobj", async (req, res) => {
+	// get the name, genre, author, and score from the form
+	const { name, genre, author, score } = req.body;
+
+	// create the custom object data
+	const update = {
+		properties: { name, genre, author, score },
+	};
+	console.log("object data", update);
+	
+	return res.redirect("/update-cobj");
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -76,6 +85,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
